c97349479f3ab1db29ee8dd8c53a6e0ab27b9171
Padraig Gleeson <p.gleeson@gmail.com>
2017-05-11 15:49:27 +0100

    Renaming HindmarshRose & regenerated

--- Brian/regenerate.sh
+++ Brian/regenerate.sh
@@ -1,3 +1,7 @@
+jnml ~/git/HindmarshRose1984/NeuroML2/LEMS_Regular_HindmarshRose.xml -brian
+jnml ~/git/HindmarshRose1984/NeuroML2/LEMS_Regular_HindmarshRose.xml -brian2
+cp ~/git/HindmarshRose1984/NeuroML2/LEMS_Regular_HindmarshRose*brian*py .
+
 jnml ~/NeuroML2/LEMSexamples/LEMS_NML2_Ex9_FN.xml -brian 
 jnml ~/NeuroML2/LEMSexamples/LEMS_NML2_Ex9_FN.xml -brian2
 cp ~/NeuroML2/LEMSexamples/LEMS_NML2_Ex9_FN_brian*.py .
