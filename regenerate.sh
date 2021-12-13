3abb8beecc192ed5db49b5ecb122de47817eed70
Padraig Gleeson <p.gleeson@gmail.com>
2021-12-13 14:49:16 +0000

    More tests

--- regenerate.sh
+++ regenerate.sh
@@ -2,10 +2,13 @@ set -e
 
 cd NeuroML2
 
+jnml Run_MorrisLecarSCell.xml -brian
+jnml Run_MorrisLecarSCell.xml -brian2
+
 jnml LEMS_Regular_HindmarshRose.xml -brian
 jnml LEMS_Regular_HindmarshRose.xml -brian2
 
-jnml LEMS_NML2_Ex9_FN.xml -brian 
+jnml LEMS_NML2_Ex9_FN.xml -brian
 jnml LEMS_NML2_Ex9_FN.xml -brian2
 
 jnml LEMS_2007One.xml -brian2
@@ -14,5 +17,6 @@ jnml LEMS_FiveCells.xml -brian2
 jnml LEMS_NML2_Ex5_DetCell.xml -brian2
 
 cp LEMS_*_brian*py ../Brian
+cp Run_*_brian*py ../Brian
 
 cd ..
