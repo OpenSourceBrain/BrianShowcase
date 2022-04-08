a83ffa5a636641e6442bd29b043e6f6719e7d76e
Padraig Gleeson <p.gleeson@gmail.com>
2022-04-08 11:22:52 +0100

    Use pynml in regenerate

--- regenerate.sh
+++ regenerate.sh
@@ -2,19 +2,19 @@ set -e
 
 cd NeuroML2
 
-jnml Run_MorrisLecarSCell.xml -brian
-jnml Run_MorrisLecarSCell.xml -brian2
+pynml Run_MorrisLecarSCell.xml -brian
+pynml Run_MorrisLecarSCell.xml -brian2
 
-jnml LEMS_Regular_HindmarshRose.xml -brian
-jnml LEMS_Regular_HindmarshRose.xml -brian2
+pynml LEMS_Regular_HindmarshRose.xml -brian
+pynml LEMS_Regular_HindmarshRose.xml -brian2
 
-jnml LEMS_NML2_Ex9_FN.xml -brian
-jnml LEMS_NML2_Ex9_FN.xml -brian2
+pynml LEMS_NML2_Ex9_FN.xml -brian
+pynml LEMS_NML2_Ex9_FN.xml -brian2
 
-jnml LEMS_2007One.xml -brian2
-jnml LEMS_FiveCells.xml -brian2
+pynml LEMS_2007One.xml -brian2
+pynml LEMS_FiveCells.xml -brian2
 
-jnml LEMS_NML2_Ex5_DetCell.xml -brian2
+pynml LEMS_NML2_Ex5_DetCell.xml -brian2
 
 cp LEMS_*_brian*py ../Brian
 cp Run_*_brian*py ../Brian
